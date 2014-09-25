f656577a7272e321657cff5073bb21748765595b
Damien Pobel <damien@pobel.fr>
2014-09-25 22:59:37 +0200

    Fixed #13: Deployment and staging

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+[ $? -ne 0 ] && exit 2
+
+SSH_CMD="ssh -i ssh/id_dsa -o 'StrictHostKeyChecking no'"
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
+    rsync -avcz -e "$SSH_CMD" web/ rb@vtt.revermont.bike:~/web/
+else
+    rsync -avcz -e "$SSH_CMD" web/ rb@vtt.revermont.bike:~/testing/$TRAVIS_PULL_REQUEST
+fi

2fddf5750b8054d132fc72715145a4e571b70d74
Damien Pobel <damien@pobel.fr>
2014-09-27 13:45:51 +0200

    Fixed the path on the remote server when deploying a PR result

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ SSH_CMD="ssh -i ssh/id_dsa -o 'StrictHostKeyChecking no'"
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
     rsync -avcz -e "$SSH_CMD" web/ rb@vtt.revermont.bike:~/web/
 else
-    rsync -avcz -e "$SSH_CMD" web/ rb@vtt.revermont.bike:~/testing/$TRAVIS_PULL_REQUEST
+    rsync -avcz -e "$SSH_CMD" web/ rb@vtt.revermont.bike:~/testing/${TRAVIS_PULL_REQUEST}.vtt.revermont.bike
 fi

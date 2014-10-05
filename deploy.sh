b00895ffeb36f396e40c7471d323b68b3fbed86b
Damien Pobel <damien@pobel.fr>
2014-10-05 23:19:39 +0200

    Added the --delete option to rsync

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 SSH_CMD="ssh -i ssh/id_dsa -o 'StrictHostKeyChecking no'"
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
-    rsync -avcz -e "$SSH_CMD" web/ rb@vtt.revermont.bike:~/web/
+    rsync -avcz --delete -e "$SSH_CMD" web/ rb@vtt.revermont.bike:~/web/
 else
-    rsync -avcz -e "$SSH_CMD" web/ rb@vtt.revermont.bike:~/testing/${TRAVIS_PULL_REQUEST}.vtt.revermont.bike
+    rsync -avcz --delete -e "$SSH_CMD" web/ rb@vtt.revermont.bike:~/testing/${TRAVIS_PULL_REQUEST}.vtt.revermont.bike
 fi

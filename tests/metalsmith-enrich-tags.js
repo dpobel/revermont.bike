3e11a7e0ed9311bd88fac875dbdd20fe9720bf54
Damien Pobel <damien@pobel.fr>
2014-10-09 21:44:23 +0200

    Fixed #74: make sure to handle tag with spaces and special characters

--- tests/metalsmith-enrich-tags.js
+++ tests/metalsmith-enrich-tags.js
@@ -21,15 +21,15 @@ describe('Metalsmith enrich tags', function () {
         };
         metadata = {
             tagsList: {
-                'tag1': {'tag': 'tag1'},
-                'notEnriched': {'tag': 'notEnriched'},
+                'tag1': {'tag': 'tag1', 'path': 'tags/tag1.md'},
+                'notEnriched': {'tag': 'notEnriched', 'path': 'tags/notEnriched.md'},
             },
         };
     });
 
     it('should add data to the tag page object', function () {
         enrichTags()(files, metalsmith, function () {
-            assert.equal(2, Object.keys(metadata.tagsList.tag1).length);
+            assert.equal(3, Object.keys(metadata.tagsList.tag1).length);
             assert.equal("tag1", metadata.tagsList.tag1.tag);
             assert.equal("tag1.md", metadata.tagsList.tag1.addition);
         });
@@ -37,7 +37,7 @@ describe('Metalsmith enrich tags', function () {
 
     it('should take the basePath parameter into account', function () {
         enrichTags({basePath: 'dir'})(files, metalsmith, function () {
-            assert.equal(2, Object.keys(metadata.tagsList.tag1).length);
+            assert.equal(3, Object.keys(metadata.tagsList.tag1).length);
             assert.equal("tag1", metadata.tagsList.tag1.tag);
             assert.equal("dir/tag1.md", metadata.tagsList.tag1.addition);
         });
@@ -45,7 +45,7 @@ describe('Metalsmith enrich tags', function () {
 
     it('should take the basePath parameter into account and clean trailing slashes', function () {
         enrichTags({basePath: 'dir/////'})(files, metalsmith, function () {
-            assert.equal(2, Object.keys(metadata.tagsList.tag1).length);
+            assert.equal(3, Object.keys(metadata.tagsList.tag1).length);
             assert.equal("tag1", metadata.tagsList.tag1.tag);
             assert.equal("dir/tag1.md", metadata.tagsList.tag1.addition);
         });
@@ -53,7 +53,7 @@ describe('Metalsmith enrich tags', function () {
 
     it('should take the ext parameter into account', function () {
         enrichTags({ext: 'html'})(files, metalsmith, function () {
-            assert.equal(2, Object.keys(metadata.tagsList.tag1).length);
+            assert.equal(3, Object.keys(metadata.tagsList.tag1).length);
             assert.equal("tag1", metadata.tagsList.tag1.tag);
             assert.equal("tag1.html", metadata.tagsList.tag1.addition);
         });
@@ -61,7 +61,7 @@ describe('Metalsmith enrich tags', function () {
 
     it('should keep intact tag page object without additional data', function () {
         enrichTags()(files, metalsmith, function () {
-            assert.equal(1, Object.keys(metadata.tagsList.notEnriched).length);
+            assert.equal(2, Object.keys(metadata.tagsList.notEnriched).length);
             assert.equal("notEnriched", metadata.tagsList.notEnriched.tag);
         });
     });

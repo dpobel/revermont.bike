23d3b72516b1897bad0427acdda6156bb6bba698
Damien Pobel <dp@ez.no>
2014-10-18 23:22:24 +0200

    Fixed #9: moved the layers init in a dedicated module

---
+++ assets/js/layers.js
@@ -0,0 +1,27 @@
+/* global L */
+(function (global, L) {
+    "use strict";
+
+    var RB = global.RB = global.RB || {},
+        attr = '&copy; <a href="http://www.ign.fr/">IGN</a>';
+
+    function layerUrl(key, layer) {
+        return "http://wxs.ign.fr/" + key
+            + "/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&"
+            + "LAYER=" + layer + "&STYLE=normal&TILEMATRIXSET=PM&"
+            + "TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fjpeg";
+    }
+
+    RB.layers = function (ignApiKey) {
+        return {
+            "IGN Scan express": L.tileLayer(
+                layerUrl(ignApiKey, "GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE"),
+                attr
+            ),
+            "IGN Topo": L.tileLayer(
+                layerUrl(ignApiKey, "GEOGRAPHICALGRIDSYSTEMS.MAPS"),
+                attr
+            )
+        };
+    };
+})(window, L);
